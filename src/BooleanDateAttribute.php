725d33e0aa1ec3cf27204c75f56cc4176e98266a
Sebastiaan Luca <hello@sebastiaanluca.com>
2022-03-13 14:07:32 +0100

    Support mixed input

--- src/BooleanDateAttribute.php
+++ src/BooleanDateAttribute.php
@@ -13,11 +13,11 @@ class BooleanDateAttribute extends Attribute
     {
         return parent::make(
             get: static fn (?string $value, array $attributes): bool => $attributes[$column] !== null,
-            set: static fn (bool $value, array $attributes): array => static::setBooleanDate($value, $attributes, $column),
+            set: static fn (mixed $value, array $attributes): array => static::setBooleanDate($value, $attributes, $column),
         );
     }
 
-    private static function setBooleanDate(bool $value, array $attributes, string $column): array
+    private static function setBooleanDate(mixed $value, array $attributes, string $column): array
     {
         // Only update the field if it's never been set before or when it's being "disabled"
 
